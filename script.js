2639f469de8b04a88e9c808c7ee8870376edf700
Mohin <mohinnadaf722@gmail.com>
2026-02-25 23:27:16 +0530

    your update message

--- script.js
+++ script.js
@@ -1,5 +1,2 @@
-// 3 log  the data type of "123",123,true, and null using typeof
-console.log(typeof "123");  // string
-console.log(typeof 123);    // number
-console.log(typeof true);   // boolean
-console.log(typeof null);   // object
+console.log(typeof "Hello World"); 
+// string
