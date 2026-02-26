59a63ad500bebd63c175f690c2b579614506ad2a
Mohin <mohinnadaf722@gmail.com>
2026-02-26 23:43:31 +0530

    Updated code

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-function calculate(a, b) {
-  return a + b;
-}
-let output = calculate(5, 7);
-console.log(typeof calculate);
+let uniqueId = Symbol("user");
+let data = {};
+data[uniqueId] = 101;
+console.log(typeof uniqueId);
