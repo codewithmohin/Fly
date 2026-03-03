b1ebf5c10f6823693867ec6d396f832a1a67ebd4
Mohin <mohinnadaf722@gmail.com>
2026-03-03 23:37:33 +0530

    change css

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-let str = "Mohin";
-let reversed = str.split("").reverse().join("");
-console.log(reversed);
+let n = 17;
+let prime = n > 1 && [...Array(n).keys()].slice(2).every(i => n % i !== 0);
+console.log(prime);
