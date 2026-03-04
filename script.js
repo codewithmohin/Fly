8e58fa8dc1ea974a834bc261bf628c6ba450d14f
Mohin <mohinnadaf722@gmail.com>
2026-03-04 22:02:38 +0530

    chnage js

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-console.log([] == ![]);  // true (![] -> false -> 0, [] -> 0)
+const obj = { 0: 'zero', 1: 'one', true: 'truth' };
+console.log(obj[true], obj[1]);  // 'truth' 'one' (true -> '1' -> 'truth')
