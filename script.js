d0c37117c4e622657a0a9cda2fd33445029f62b0
Mohin <mohinnadaf722@gmail.com>
2026-03-04 22:04:19 +0530

    chnage js

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-console.log(NaN === NaN);  // false
-console.log(Number.isNaN(NaN));  // true
+console.log(0.1 + 0.2 === 0.3);  // false (0.30000000000000004)
+console.log((0.1 + 0.2).toPrecision(10));  // '0.3000000000'
