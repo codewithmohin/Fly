3f795e8401a88d954ba94cf1e50cfa8f393d3611
Mohin <mohinnadaf722@gmail.com>
2026-03-04 22:06:20 +0530

    chnage js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-outer: for (let i = 0; i < 3; i++) {
-  for (let j = 0; j < 3; j++) {
-    if (i === 1 && j === 1) break outer;
-    console.log(i, j);
-  }
-}  // Stops at 1,1
+function* selfYield() {
+  yield* selfYield();
+}
+const gen = selfYield();
+console.log(gen.next());  // {value: Generator, done: false} (infinite!)
