e06c2e8ce0b13859993fe3c4f301722e9762d6b2
Mohin <mohinnadaf722@gmail.com>
2026-03-04 22:03:16 +0530

    chnage js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const setA = new Set([1,2,3]);
-const setB = new Set([2,3,4]);
-const union = new Set([...setA, ...setB]);  // Proposal: setA.union(setB)
-console.log(union);  // Set {1,2,3,4}
+// Polyfill or future native
+const now = Temporal.Now.plainDateTimeISO();
+const nextWeek = now.add({ weeks: 1 });
+console.log(nextWeek.toString());
