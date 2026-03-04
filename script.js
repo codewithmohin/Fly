25e497f1d22b62d663678082601d21aef9bfe060
Mohin <mohinnadaf722@gmail.com>
2026-03-04 22:03:52 +0530

    chnage js

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
-// Polyfill or future native
-const now = Temporal.Now.plainDateTimeISO();
-const nextWeek = now.add({ weeks: 1 });
-console.log(nextWeek.toString());
+console.log(NaN === NaN);  // false
+console.log(Number.isNaN(NaN));  // true
