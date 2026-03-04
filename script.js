316355d49fe6394d4359824612efef0928a968db
Mohin <mohinnadaf722@gmail.com>
2026-03-04 22:02:06 +0530

    chnage js

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-const nums = [3, 1, 2];
-const sorted = nums.toSorted((a, b) => a - b);  // [1, 2, 3], nums unchanged
-const reversed = nums.toReversed();  // [2, 1, 3], nums unchanged
+const { promise, resolve, reject } = Promise.withResolvers();
+setTimeout(() => resolve('Done!'), 1000);
+promise.then(console.log);  // 'Done!' after 1s
