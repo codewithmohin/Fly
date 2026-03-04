2c0e40c36350df416fcd38d1f9abb5374c52fe7a
Mohin <mohinnadaf722@gmail.com>
2026-03-04 22:02:22 +0530

    chnage js

--- script.js
+++ script.js
@@ -1,3 +1,1 @@
-const { promise, resolve, reject } = Promise.withResolvers();
-setTimeout(() => resolve('Done!'), 1000);
-promise.then(console.log);  // 'Done!' after 1s
+console.log([] == ![]);  // true (![] -> false -> 0, [] -> 0)
