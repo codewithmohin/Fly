79d0c6c6bc46b9eff2069cedcdd3479d50d0dae6
Mohin <mohinnadaf722@gmail.com>
2026-03-05 22:30:07 +0530

    update javascript code

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-const ball=document.createElement("div")
-Object.assign(ball.style,{width:"50px",height:"50px",background:"red",borderRadius:"50%",position:"absolute"})
-document.body.appendChild(ball)
-let x=0,y=0,dx=3,dy=3
-function move(){
-x+=dx
-y+=dy
-if(x>window.innerWidth-50||x<0)dx*=-1
-if(y>window.innerHeight-50||y<0)dy*=-1
-ball.style.transform=`translate(${x}px,${y}px)`
-requestAnimationFrame(move)
-}
-move()
+document.addEventListener("mousemove",e=>{
+const s=document.createElement("div")
+Object.assign(s.style,{width:"8px",height:"8px",borderRadius:"50%",background:`hsl(${Math.random()*360},100%,60%)`,position:"absolute",left:e.pageX+"px",top:e.pageY+"px",pointerEvents:"none",transition:"all 1s linear"})
+document.body.appendChild(s)
+setTimeout(()=>{s.style.transform="scale(0)";s.style.opacity="0"},10)
+setTimeout(()=>s.remove(),1000)
+})
