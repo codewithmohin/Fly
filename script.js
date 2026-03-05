d19dea95e779dbe06d444198ed739c09e1051695
Mohin <mohinnadaf722@gmail.com>
2026-03-05 22:22:06 +0530

    update javascript code

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const flatten = a => a.flat(Infinity);
-console.log(flatten([1,[2,[3,4],5]]));
+const count = s => [...s].reduce((a,c)=>({...a,[c]:(a[c]||0)+1}),{});
+console.log(count("javascript"));
