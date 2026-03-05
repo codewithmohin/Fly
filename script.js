c1ec2c8a9ec72b6d3a8b4793d890cac78558130e
Mohin <mohinnadaf722@gmail.com>
2026-03-05 22:29:37 +0530

    update javascript code

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(0.1 + 0.2)
+for(let i=0;i<30;i++){
+const c=document.createElement("div")
+Object.assign(c.style,{width:"20px",height:"20px",borderRadius:"50%",position:"absolute",background:`hsl(${Math.random()*360},70%,60%)`,left:Math.random()*window.innerWidth+"px",top:Math.random()*window.innerHeight+"px"})
+document.body.appendChild(c)
+let dy=Math.random()*2+1
+function float(){
+let y=parseFloat(c.style.top)
+y-=dy
+if(y<0)y=window.innerHeight
+c.style.top=y+"px"
+requestAnimationFrame(float)
+}
+float()
+}
