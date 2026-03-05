d58c3fb1fb3a4439b79d3ca6ece21d1d5e0e9ab6
Mohin <mohinnadaf722@gmail.com>
2026-03-05 22:20:32 +0530

    update javascript code

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-console.log((0.595).toFixed(2));  // '0.59' (rounds down unexpectedly)
-console.log((0.605).toFixed(2));  // '0.61'
+const reverse = s => [...s].reverse().join('');
+console.log(reverse("magic"));
