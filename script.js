aa08ec8b4d5fd848d733b57aa53b4fe265f6d459
Mohin <mohinnadaf722@gmail.com>
2026-03-06 22:29:02 +0530

    updated  js code

--- script.js
+++ script.js
@@ -1,24 +1,5 @@
-<canvas id="c"></canvas>
-<script>
-const c=document.getElementById("c")
-const ctx=c.getContext("2d")
-c.width=innerWidth
-c.height=innerHeight
-
-let a=0
-
-function draw(){
-ctx.clearRect(0,0,c.width,c.height)
-
-ctx.save()
-ctx.translate(c.width/2,c.height/2)
-ctx.rotate(a)
-ctx.fillRect(-50,-50,100,100)
-ctx.restore()
-
-a+=0.02
-requestAnimationFrame(draw)
+function reverseMagic(n){
+let r=parseInt(String(n).split('').reverse().join(''))
+return n+r
 }
-
-draw()
-</script>
+console.log(reverseMagic(123))
