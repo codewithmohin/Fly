848dd3c0ca6953d90d6934f745b8b3369649cd3b
Mohin <mohinnadaf722@gmail.com>
2026-03-06 22:30:53 +0530

    updated  js code

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-function isPrime(n){
-for(let i=2;i<=Math.sqrt(n);i++)
-if(n%i===0)return false
-return n>1
+function fact(n){
+return n<=1?1:n*fact(n-1)
 }
-console.log(isPrime(17))
+console.log(fact(6))
