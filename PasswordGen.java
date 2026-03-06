df5fce45a64e328c23683deb369b108d19a57c4e
Mohin <mohinnadaf722@gmail.com>
2026-03-06 09:01:28 +0530

    password genarator

---
+++ PasswordGen.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+
+public class PasswordGen {
+    public static void main(String[] args) {
+        String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#";
+        Random r = new Random();
+        String pass = "";
+
+        for(int i=0;i<10;i++){
+            pass += chars.charAt(r.nextInt(chars.length()));
+        }
+
+        System.out.println(pass);
+    }
+}
