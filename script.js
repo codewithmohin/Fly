e17fbfaf9d5a801a46130751bfa4e159fe25a2e3
Mohin <mohinnadaf722@gmail.com>
2026-03-06 22:27:50 +0530

    updated html and js code

--- script.js
+++ script.js
@@ -1,26 +1,24 @@
-const text="Hi, I'm Mohin 👋"
-let i=0
-function type(){
-if(i<text.length){
-document.getElementById("typing").innerHTML+=text.charAt(i)
-i++
-setTimeout(type,80)
+<canvas id="c"></canvas>
+<script>
+const c=document.getElementById("c")
+const ctx=c.getContext("2d")
+c.width=innerWidth
+c.height=innerHeight
+
+let a=0
+
+function draw(){
+ctx.clearRect(0,0,c.width,c.height)
+
+ctx.save()
+ctx.translate(c.width/2,c.height/2)
+ctx.rotate(a)
+ctx.fillRect(-50,-50,100,100)
+ctx.restore()
+
+a+=0.02
+requestAnimationFrame(draw)
 }
-}
-type()
 
-const grid=document.getElementById("grid")
-
-for(let i=0;i<364;i++){
-const cell=document.createElement("div")
-cell.classList.add("cell")
-
-const r=Math.floor(Math.random()*5)
-
-if(r===1)cell.classList.add("level1")
-if(r===2)cell.classList.add("level2")
-if(r===3)cell.classList.add("level3")
-if(r===4)cell.classList.add("level4")
-
-grid.appendChild(cell)
-}
+draw()
+</script>
