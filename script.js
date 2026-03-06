16c009b23290c6fd1f3eb68798cfa95b0b842af7
Mohin <mohinnadaf722@gmail.com>
2026-03-06 22:49:56 +0530

    updated  js code

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let sum=0
-for(let i=1;i<=10;i++){
-sum+=i
-console.log(sum)
+function kaprekar(n){
+let s=n*n+""
+let mid=Math.floor(s.length/2)
+return Number(s.slice(0,mid))+Number(s.slice(mid))==n
 }
+console.log(kaprekar(45))
