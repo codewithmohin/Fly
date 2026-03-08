f395f47f6e50e912066cafceed83e8650fefa630
Mohin <mohinnadaf722@gmail.com>
2026-03-08 23:21:13 +0530

    code

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-let x = 50;
-
-if (x <= 50) {
-    if (x === 50) {
-        console.log("x is 50");
-    } else {
-        console.log("x is less than 50");
-    }
-} else {
-    console.log("x is greater than 50");
-}
+let str = "developer";
+let reversed = str.split("").reverse().join("");
+console.log(reversed);
