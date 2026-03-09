4c38494bf0137d4bf416707a5db629afd85c9911
Mohin <mohinnadaf722@gmail.com>
2026-03-09 10:44:45 +0530

    code add in js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var h1 = document.querySelector("h1");
 
 h1.addEventListener("click", function () {
-    alert("You clicked the heading!");
+    document.body.style.backgroundColor = "lightyellow";
 });
