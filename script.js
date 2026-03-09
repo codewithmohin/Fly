1e4162da8d3c010df7115cc047fb83e96f4c3f02
Mohin <mohinnadaf722@gmail.com>
2026-03-09 10:49:44 +0530

    code add in js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var h1 = document.querySelector("h1");
 
-h1.addEventListener("dblclick", function () {
-    h1.innerHTML = "You double clicked!";
+h1.addEventListener("mouseover", function () {
+    h1.style.color = "green";
 });
