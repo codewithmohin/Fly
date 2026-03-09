e48e2d26db35ffea02bf0d42c18be5113b65dc57
Mohin <mohinnadaf722@gmail.com>
2026-03-09 10:44:06 +0530

    code add in js

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 var h1 = document.querySelector("h1");
 
 h1.addEventListener("click", function () {
-    var colors = ["red", "blue", "green", "purple", "orange"];
-    var random = Math.floor(Math.random() * colors.length);
-    h1.style.color = colors[random];
+    h1.style.fontSize = "60px";
 });
