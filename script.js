688f08f3f7271cb39614b6d34fab9f0d62aed508
Mohin <mohinnadaf722@gmail.com>
2026-03-09 10:43:39 +0530

    code add in js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var h1 = document.querySelector("h1");
 
 h1.addEventListener("click", function () {
-    h1.style.color = "blue";
+    if (h1.innerHTML === "Hello") {
+        h1.innerHTML = "Welcome!";
+    } else {
+        h1.innerHTML = "Hello";
+    }
 });
