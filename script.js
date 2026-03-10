44da4ed60f3531098940506889ff43b535eb5c53
Mohin <mohinnadaf722@gmail.com>
2026-03-10 23:02:47 +0530

    code updated

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-// 4. Sum numbers 1 to 10
-let sum=0; for(let i=1; i<=10; i++) sum+=i; console.log(sum); // 55[web:10]
+// 5. Calculate 5!
+let fact=1; for(let i=1; i<=5; i++) fact*=i; console.log(fact); // 120[web:10]
