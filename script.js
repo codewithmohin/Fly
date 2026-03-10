d00af0d3a73ad90c64db6c3b842317b4c4b1c93a
Mohin <mohinnadaf722@gmail.com>
2026-03-10 23:11:43 +0530

    code updated

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-// 33. Sum of squares 1-10
-let sqSum = 0;
-for(let i=1; i<=10; i++) sqSum += i*i;
-console.log(sqSum); // 385[web:11]
+// 34. Perfect number check (28)
+function isPerfect(n){
+  let sum=1;
+  for(let i=2; i<n; i++) if(n%i===0) sum+=i;
+  console.log(sum === n ? `${n} is perfect` : `${n} not perfect`);
+}
+isPerfect(28); // 28 is perfect[web:11]
