00cd60df93df580fe1955b7155164631383d88d1
Mohin <mohinnadaf722@gmail.com>
2026-03-10 23:02:33 +0530

    code updated

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-// 3. Print even numbers 0 to 20
-for(let i=0; i<=20; i+=2) console.log(i);[web:16]
+// 4. Sum numbers 1 to 10
+let sum=0; for(let i=1; i<=10; i++) sum+=i; console.log(sum); // 55[web:10]
