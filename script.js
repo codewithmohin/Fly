7eac7751385baaae7d8f2c48cb4a67115f916dbd
Mohin <mohinnadaf722@gmail.com>
2026-03-10 23:08:08 +0530

    code updated

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-// 24. Armstrong number check (153)
-function isArmstrong(n){let sum=0,orig=n; while(n>0){let d=n%10; sum+=d**3; n=Math.floor(n/10);} return sum===orig;} console.log(isArmstrong(153)); // true[web:11]
+// 25. Swap two numbers without temp
+let x=5,y=10; [x,y]=[y,x]; console.log(x,y); // 10 5[web:11]
