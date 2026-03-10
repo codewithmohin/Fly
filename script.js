6c545a649f05b50a1a182b5fa4149628c52e665c
Mohin <mohinnadaf722@gmail.com>
2026-03-10 23:15:15 +0530

    code updated

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-// 44. Nth Fibonacci
-function fib(n){
-  if(n<=1) return n;
-  return fib(n-1)+fib(n-2);
+// 45. Generate primes up to 20 (Sieve snippet)
+const primes = [];
+for(let i=2; i<=20; i++){
+  let isPrime=true;
+  for(let j=2; j<i; j++) if(i%j===0){isPrime=false; break;}
+  if(isPrime) primes.push(i);
 }
-console.log(fib(8)); // 21[web:11]
+console.log(primes); // [2,3,5,7,11,13,17,19][web:11]
