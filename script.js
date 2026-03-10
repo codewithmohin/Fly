15b46f785b72cd8ac1b7276b58e13427014c13c5
Mohin <mohinnadaf722@gmail.com>
2026-03-10 23:12:45 +0530

    code updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// 34. Perfect number check (28)
-function isPerfect(n){
-  let sum=1;
-  for(let i=2; i<n; i++) if(n%i===0) sum+=i;
-  console.log(sum === n ? `${n} is perfect` : `${n} not perfect`);
+// 35. Harshad number (sum of digits divides number)
+function isHarshad(n){
+  let sum=0, num=n;
+  while(num>0){sum += num%10; num=Math.floor(num/10);}
+  console.log(n % sum === 0 ? `${n} is Harshad` : 'Not');
 }
-isPerfect(28); // 28 is perfect[web:11]
+isHarshad(18); // 18 is Harshad[web:11]
