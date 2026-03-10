3fead3a74ad61a671892409c9f0bc0d3dc278757
Mohin <mohinnadaf722@gmail.com>
2026-03-10 23:08:54 +0530

    code updated

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-// 26. Count digits
-function countDigits(n){return Math.floor(Math.log10(n))+1;} console.log(countDigits(1234)); // 4[web:11]
+// 27. Palindrome number
+function isNumPalindrome(n){return n.toString()===n.toString().split('').reverse().join('');} console.log(isNumPalindrome(121)); // true[web:6]
