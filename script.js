f06886174cb188089838d8a38abb74d3f533794f
Mohin <mohinnadaf722@gmail.com>
2026-03-12 19:08:54 +0530

    js updated

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-// 48. Count primes below 100 (simple)
-let primeCount=0;
-for(let i=2; i<100; i++){
-  let p=true;
-  for(let j=2; j<i; j++) if(i%j===0){p=false;break;}
-  if(p) primeCount++;
+function outer(){
+  let x = 10
+
+  return function inner(){
+    console.log(x)
+  }
 }
-console.log(primeCount); // 25[web:11]
+
+let f = outer()
+f()
