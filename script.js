77ecc7063124b3a287e3669e4f17a53c1a77cd6a
Mohin <mohinnadaf722@gmail.com>
2026-03-14 21:38:04 +0530

    js changed

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-let btn = document.querySelector("button");
+let colors = [
+"linear-gradient(to right,#6a11cb,#2575fc)",
+"linear-gradient(to right,#ff512f,#dd2476)",
+"linear-gradient(to right,#00c6ff,#0072ff)"
+];
+
+let i = 0;
 
 setInterval(()=>{
-btn.style.transform = "scale(1.1)";
-setTimeout(()=>{
-btn.style.transform = "scale(1)";
-},300);
-},2000);
+document.body.style.background = colors[i];
+i = (i + 1) % colors.length;
+},3000);
