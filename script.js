ab7918c96571d9ff4a4a08663ac2e12039909026
Mohin <mohinnadaf722@gmail.com>
2026-03-17 23:11:23 +0530

    code jss changed

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// 8. JSON viewer tree (console)
-function tree(obj, prefix='') {
-  Object.entries(obj).forEach(([k,v]) => {
-    console.log(prefix + k + ':', typeof v==='object' ? '{...}' : v);
-    if(typeof v==='object') tree(v, prefix+'  ');
-  });
-}
-tree({a:1, b:{c:2, d:[3,4]}});
+// 9. URL params to object
+const url = new URL('https://ex.com?a=1&b=2&c=3');
+console.log(Object.fromEntries(url.searchParams));
