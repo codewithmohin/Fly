ac2fd3d9b75100862d2b9455a6dc91880f82c0af
Mohin <mohinnadaf722@gmail.com>
2026-03-17 23:12:02 +0530

    code jss changed

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-// 11. Typewriter effect (add <p id="type"></p>)
-function typeWrite(text, el=document.getElementById('type'), speed=50) {
-  let i=0;
-  function type() { if(i<text.length) { el.textContent += text[i++]; setTimeout(type,speed); } }
-  type();
+// 12. LRU Cache (size 3)
+class LRUCache {
+  constructor(size) { this.size=size; this.cache={}; this.keys=[]; }
+  get(key) {
+    if(this.cache[key]) {
+      this.keys.splice(this.keys.indexOf(key),1);
+      this.keys.push(key);
+      return this.cache[key];
+    }
+  }
+  set(key,val) {
+    if(this.get(key)) this.cache[key]=val;
+    else {
+      if(this.keys.length >= this.size) delete this.cache[this.keys.shift()];
+      this.cache[key]=val; this.keys.push(key);
+    }
+  }
 }
-typeWrite('Hello Unique JS!');
+const cache = new LRUCache(3); cache.set('a',1); cache.set('b',2); cache.set('c',3); cache.set('d',4); console.log(cache.get('a')); // 1, evicts 'b'
