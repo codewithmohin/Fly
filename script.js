fa59531e58ea0431f6317d9e145c57d43079c52f
Mohin <mohinnadaf722@gmail.com>
2026-03-17 23:09:10 +0530

    code jss changed

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-let colors = [
-"linear-gradient(to right,#6a11cb,#2575fc)",
-"linear-gradient(to right,#ff512f,#dd2476)",
-"linear-gradient(to right,#00c6ff,#0072ff)"
-];
-
-let i = 0;
-
-setInterval(()=>{
-document.body.style.background = colors[i];
-i = (i + 1) % colors.length;
-},3000);
+// 1. Animated circle (needs <canvas id="c" width="200" height="200"></canvas>)
+const canvas = document.getElementById('c');
+const ctx = canvas.getContext('2d');
+let angle = 0;
+function draw() {
+  ctx.clearRect(0,0,200,200);
+  ctx.beginPath();
+  ctx.arc(100,100,50+20*Math.sin(angle),0,Math.PI*2);
+  ctx.fillStyle = `hsl(${angle*2},50%,50%)`;
+  ctx.fill();
+  angle += 0.05;
+  requestAnimationFrame(draw);
+}
+draw();
