68ac6667576474f4ffcc23fd0edfc2f2fa433444
Mohin <mohinnadaf722@gmail.com>
2026-03-17 23:13:54 +0530

    code jss changed

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// 19. Cellular automaton (Game of Life step)
-function lifeStep(grid) {
-  const h=grid.length, w=grid[0].length, newGrid=grid.map(r=>r.slice());
-  for(let y=1;y<h-1;y++) for(let x=1;x<w-1;x++) {
-    let n=grid[y-1][x-1]+grid[y-1][x]+grid[y-1][x+1]+grid[y][x-1]+grid[y][x+1]+grid[y+1][x-1]+grid[y+1][x]+grid[y+1][x+1];
-    newGrid[y][x] = grid[y][x] ? (n===2||n===3?1:0) : (n===3?1:0);
-  }
-  return newGrid;
+// 20. Fractal tree (recursive console)
+function tree(n, prefix='') {
+  if(n<1) return;
+  console.log(prefix + '🌳');
+  tree(n-1, prefix+'│  ');
+  console.log(prefix + '└──');
+  tree(n-1, prefix+'   ');
 }
-let g = [[0,0,0,0,0],[0,0,1,0,0],[0,0,0,1,0],[0,1,1,1,0],[0,0,0,0,0]];
-console.table(lifeStep(g));
+tree(4);
