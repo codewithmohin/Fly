11d8cf2d7e8cd363839982d973b015495657182f
Mohin <mohinnadaf722@gmail.com>
2026-03-17 23:12:21 +0530

    code jss changed

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-// 12. LRU Cache (size 3)
-class LRUCache {
-  constructor(size) { this.size=size; this.cache={}; this.keys=[]; }
-  get(key) {
-    if(this.cache[key]) {
-      this.keys.splice(this.keys.indexOf(key),1);
-      this.keys.push(key);
-      return this.cache[key];
-    }
+// 13. Stack with max
+class StackWithMax {
+  constructor() { this.items=[]; this.maxs=[]; }
+  push(val) {
+    this.items.push(val);
+    this.maxs.push(this.maxs.length ? Math.max(this.maxs[this.maxs.length-1], val) : val);
   }
-  set(key,val) {
-    if(this.get(key)) this.cache[key]=val;
-    else {
-      if(this.keys.length >= this.size) delete this.cache[this.keys.shift()];
-      this.cache[key]=val; this.keys.push(key);
-    }
-  }
+  pop() { this.items.pop(); this.maxs.pop(); }
+  max() { return this.maxs[this.maxs.length-1]; }
 }
-const cache = new LRUCache(3); cache.set('a',1); cache.set('b',2); cache.set('c',3); cache.set('d',4); console.log(cache.get('a')); // 1, evicts 'b'
