0dbc5d89b3c36b2b74a5caa75dec789bb02795b5
Mohin <mohinnadaf722@gmail.com>
2026-03-18 20:57:36 +0530

    code added

--- script.js
+++ script.js
@@ -1,4 +1,2 @@
-function toggleClass(el, className) {
-  el.classList.toggle(className);
-}
-// Usage: toggleClass(document.querySelector('button'), 'active');
+const shuffle = arr => arr.sort(() => 0.5 - Math.random());
+console.log(shuffle([1, 2, 3, 4, 5])); // e.g., [3, 1, 5, 2, 4]
