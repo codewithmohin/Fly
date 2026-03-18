6ae67cf4fe7221fb387f61ae0c82037516b15fe6
Mohin <mohinnadaf722@gmail.com>
2026-03-18 20:55:51 +0530

    code added

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
- function tree(n, prefix='') {
-  if(n<1) return;
-  console.log(prefix + '🌳');
-  tree(n-1, prefix+'│  ');
-  console.log(prefix + '└──');
-  tree(n-1, prefix+'   ');
-}
-tree(4);
+const nums = Array.from({length: 20}, (_, i) => i + 1);
+const evenSum = nums.filter(n => n % 2 === 0).reduce((a, b) => a + b, 0);
+const oddsAbove10 = nums.filter(n => n % 2 && n > 10);
+console.log(evenSum, oddsAbove10); // 110 [11, 13, 15, 17, 19]
