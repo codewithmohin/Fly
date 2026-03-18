9e1277f3149b18aa39932d46fb0a82d2d60b0dbb
Mohin <mohinnadaf722@gmail.com>
2026-03-18 20:57:57 +0530

    code added

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-const shuffle = arr => arr.sort(() => 0.5 - Math.random());
-console.log(shuffle([1, 2, 3, 4, 5])); // e.g., [3, 1, 5, 2, 4]
+function debounce(fn, delay) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => fn(...args), delay);
+  };
+}
+const debouncedLog = debounce(console.log, 300);
