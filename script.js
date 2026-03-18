9ea1373ff82f0ca96da9dc0737d3c08ce608a4fc
Mohin <mohinnadaf722@gmail.com>
2026-03-18 20:58:54 +0530

    code added

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-const curryAdd = a => b => c => a + b + c;
-console.log(curryAdd(10)(20)(30)); // 60
+function fib(n) {
+  if (n <= 1) return [0, 1].slice(0, n + 1);
+  const seq = fib(n - 1);
+  seq.push(seq[seq.length - 1] + seq[seq.length - 2]);
+  return seq;
+}
+console.log(fib(8)); // [0, 1, 1, 2, 3, 5, 8, 13, 21]
