324d2b6bb6782c5cb03c9eda45c0cda9c638044e
Mohin <mohinnadaf722@gmail.com>
2026-03-18 20:58:25 +0530

    code added

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-function debounce(fn, delay) {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => fn(...args), delay);
-  };
-}
-const debouncedLog = debounce(console.log, 300);
+const users = [{id:1, active:true}, {id:2, active:false}];
+const activeIds = users
+  .filter(u => u.active)
+  .map(u => u.id)
+  .find(id => id > 0);
+console.log(activeIds); // 1
