2430016c83e91ef50c8f6fc9526d30973b99461c
Mohin <mohinnadaf722@gmail.com>
2026-03-20 22:09:03 +0530

    html code added

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-function sentiment(text) {
-    if (text.includes("good")) return "Positive 😊";
-    if (text.includes("bad")) return "Negative 😡";
-    return "Neutral 😐";
+function autoReply(msg) {
+    const replies = {
+        "hi": "Hello 👋",
+        "price": "It costs ₹99/month",
+        "help": "How can I help you?"
+    };
+
+    return replies[msg.toLowerCase()] || "Please ask something else";
 }
 
-console.log(sentiment("This is good"));
+console.log(autoReply("hi"));
