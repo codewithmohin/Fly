caa8a2b30d3c1df11c1a68d96a388999a252b57d
Mohin <mohinnadaf722@gmail.com>
2026-03-24 23:29:24 +0530

    js code updated

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-function addMessage(text, type="bot") {
+function scrollToBottom() {
     let chatBox = document.getElementById("chat-box");
-    let div = document.createElement("div");
-
-    div.style.color = type === "user" ? "#38bdf8" : "#22c55e";
-    div.innerText = "> " + text;
-
-    chatBox.appendChild(div);
+    chatBox.scrollTop = chatBox.scrollHeight;
 }
