ca5506b916f0b9d820d474b094049c0696231f39
Mohin <mohinnadaf722@gmail.com>
2026-03-26 09:03:04 +0530

    goo1

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const { execSync } = require("child_process");
 const clear = () => execSync("clear || cls");
 
-const chars = "0123456789@#$%^&*";
-let cols = Array.from({ length: 40 }, () => 0);
+const shapes = [
+  "  ╭─────╮  ",
+  "  │  3D │  ",
+  "  ╰─────╯  ",
+];
+
+let t = 0;
 setInterval(() => {
   clear();
-  let s = Array(20).fill(0).map((_, y) =>
-    cols.map((x, cx) => x === y ? chars[Math.random() * chars.length | 0] : " ").join("")
-  ).join("\n");
-  console.log(s);
-  cols = cols.map((x) => (x + 1 + Math.random() * 2) % 20 | 0);
-}, 200);
+  const r = t % 4;
+  const rotated = [
+    shapes[r % 2],
+    shapes[(r + 1) % 2],
+    shapes[(r + 2) % 2],
+    shapes[(r + 3) % 2],
+  ];
+  console.log(rotated.join("\n"));
+  t++;
+}, 300);
