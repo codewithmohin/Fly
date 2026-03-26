b9e2b318c40c1e2de677b89eff0672bd5da7e288
Mohin <mohinnadaf722@gmail.com>
2026-03-26 22:13:51 +0530

    mmmmmmooohhhhiin

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-let t=0;setInterval(()=>{console.clear();for(let y=0;y<20;y++){let l="";for(let x=0;x<40;x++)l+=Math.cos(x+t*y)>0?"#":" ";console.log(l)}t+=0.1},60);
+let t=0;setInterval(()=>{console.clear();for(let y=0;y<20;y++){let l="";for(let x=0;x<40;x++)l+=Math.sin(x+y+t)>0?"█":" ";console.log(l)}t+=0.1},60);
