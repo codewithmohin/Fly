e68f79b9fc3b610c4874462887b77318046e6800
Mohin <mohinnadaf722@gmail.com>
2026-03-26 22:11:08 +0530

    mmmmmmooohhhhiin

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ setInterval(() => {
   for (let y = -10; y < 10; y++) {
     let line = "";
     for (let x = -20; x < 20; x++) {
-      let d = Math.sqrt(x*x + y*y);
-      line += Math.sin(d - t) > 0 ? "#" : " ";
+      let d = x*x + y*y;
+      line += d < 100 ? (Math.sin(t + d*0.05) > 0 ? "@" : ".") : " ";
     }
     console.log(line);
   }
-  t += 0.3;
-}, 60);
+  t += 0.2;
+}, 70);
